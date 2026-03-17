044a40a2deca18315630e6626e439bc4b530a04f
RonaldoCMS <ronaldocms@outlook.it>
2026-03-17 22:59:36 +0100

    upd

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -15,8 +15,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    // origin: process.env.FRONTEND_URL,
+    // credentials: true,
     origin: '*',
-    credentials: true,
   });
 
   // Serve uploaded files as static assets
